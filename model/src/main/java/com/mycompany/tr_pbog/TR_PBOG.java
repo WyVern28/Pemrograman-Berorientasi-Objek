b2e06447abbb9770982381a456106f1bfc11ace3
NatanaelWM <nathan.kudet@gmail.com>
2025-10-27 15:56:57 +0700

    multi module

---
+++ model/src/main/java/com/mycompany/tr_pbog/TR_PBOG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tr_pbog;
+
+/**
+ *
+ * @author Windows
+ */
+public class TR_PBOG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
