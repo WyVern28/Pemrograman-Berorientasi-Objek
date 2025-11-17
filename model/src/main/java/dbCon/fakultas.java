9050feb49a31c36e3e19c8f9d0ee6d6beb0d2df7
Andaru Hanif Pergita <161188736+LuckiestDaru@users.noreply.github.com>
2025-11-17 10:44:32 +0700

    update db_config

---
+++ model/src/main/java/dbCon/fakultas.java
@@ -0,0 +1,21 @@
+package dbCon;
+
+public class fakultas {
+    public String id_fakultas, nama_fakultas;
+
+    public String getId_fakultas() {
+        return id_fakultas;
+    }
+
+    public void setId_fakultas(String id_fakultas) {
+        this.id_fakultas = id_fakultas;
+    }
+
+    public String getNama_fakultas() {
+        return nama_fakultas;
+    }
+
+    public void setNama_fakultas(String nama_fakultas) {
+        this.nama_fakultas = nama_fakultas;
+    }
+}
---
+++ model/src/main/java/dbCon/mahasiswa.java
@@ -0,0 +1,47 @@
+package dbCon;
+
+public class mahasiswa {
+    private String nim, nama, id_prodi;
+    private float ipk;
+    private int totalSks;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getId_prodi() {
+        return id_prodi;
+    }
+
+    public void setId_prodi(String id_prodi) {
+        this.id_prodi = id_prodi;
+    }
+
+    public float getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(float ipk) {
+        this.ipk = ipk;
+    }
+
+    public int getTotalSks() {
+        return totalSks;
+    }
+
+    public void setTotalSks(int totalSks) {
+        this.totalSks = totalSks;
+    }
+}
