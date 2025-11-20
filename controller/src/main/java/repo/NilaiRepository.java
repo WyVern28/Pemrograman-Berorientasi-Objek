1f7092773f887ecd05cc665e81b7f9181de8322a
NatanaelWM <nathan.kudet@gmail.com>
2025-11-20 15:23:37 +0700

    betulin bug dosen ga bisa lihat mahasiswa dikelasnya

--- controller/src/main/java/repo/NilaiRepository.java
+++ controller/src/main/java/repo/NilaiRepository.java
@@ -7,21 +7,26 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import DTO.LihatMahasiswa;
 import DTO.TranskripNilai;
-import dbCon.Mahasiswa;
 import dbCon.db_config;
 
 public class NilaiRepository {
-    public List<Mahasiswa> getMahasiswaByIdKelas(String idKelas){
-        MahasiswaRepository mhsRepo = new MahasiswaRepository();
-        List<Mahasiswa> listMahasiswa = new ArrayList<>();
-        String sql = "select nim from nilai where id_kelas = ? and status = false";
+    public List<LihatMahasiswa> getMahasiswaByIdKelas(String idKelas){
+        List<LihatMahasiswa> listMahasiswa = new ArrayList<>();
+        String sql = """
+                select nilai.nim,mahasiswa.name from kelas
+                inner join nilai on kelas.id_kelas = nilai.id_kelas
+                inner join mahasiswa on nilai.nim = mahasiswa.nim
+                where kelas.id_kelas = ? and status = false
+                """;;
         try(Connection conn = db_config.getConn();PreparedStatement prep = conn.prepareStatement(sql)) {
             prep.setString(1, idKelas);
             ResultSet res = prep.executeQuery();
             while (res.next()) {
-                String nim = res.getString("nim");
-                listMahasiswa.add(mhsRepo.getMahasiswaById(nim));
+                listMahasiswa.add(new LihatMahasiswa(res.getString("nim"),
+                res.getString("name"))
+            );
             }
         } catch (SQLException e) {
             // TODO: handle exception
---
+++ model/src/main/java/DTO/LihatMahasiswa.java
@@ -0,0 +1,23 @@
+package DTO;
+
+public class LihatMahasiswa {
+    private String nama;
+    private String nim;
+    public LihatMahasiswa(String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getNim() {
+        return nim;
+    }
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+}
