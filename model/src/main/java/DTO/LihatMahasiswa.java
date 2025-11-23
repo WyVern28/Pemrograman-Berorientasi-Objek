413548eb06b0cfaeec796b6ced1352cedbdd604e
NatanaelWM <nathan.kudet@gmail.com>
2025-11-23 07:29:12 +0700

    betulin constructor LihatMahasiswa

--- model/src/main/java/DTO/LihatMahasiswa.java
+++ model/src/main/java/DTO/LihatMahasiswa.java
@@ -3,7 +3,7 @@ package DTO;
 public class LihatMahasiswa {
     private String nama;
     private String nim;
-    public LihatMahasiswa(String nama, String nim) {
+    public LihatMahasiswa(String nim, String nama) {
         this.nama = nama;
         this.nim = nim;
     }
